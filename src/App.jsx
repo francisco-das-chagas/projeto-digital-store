0779746fad2c89db6cd99f66bcb66e3d79f44a6a
Francisco Das Chagas <66851227+francisco-das-chagas@users.noreply.github.com>
2025-06-12 19:53:06 -0300

    adicionando css

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,8 @@ import ProductListingPage from "./pages/ProductListingPage";
 import ProductViewPage from "./pages/ProductViewPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import UnderContructionPage from "./pages/UnderConstructionPage";
-import {createGlobalStyle} from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import { SiteData } from "./data";
-
 
 export const GlobalStyle = createGlobalStyle`
 
@@ -20,7 +19,37 @@ export const GlobalStyle = createGlobalStyle`
 
    body {
       background-color: var(--gray-50);
-      
+      margin: 0;
+      padding: 0;
+      font-family: 'Segoe UI', Tahoma, sans-serif;
+      line-height: 1.4;
+      overflow-x: hidden;
    }
 
+   a {
+      text-decoration: none;
+      color: inherit;
+   }
+   ul, ol {
+      padding: 0;
+      margin: 0;
+      list-style: none;
+   }
+   img {
+      width: 100%;
+      height: auto;
+   }
+
+   .max-w-36rem {max-width: 36rem}
+   .max-w-75rem {max-width: 75rem}
+   .grid-cols-3 {grid-template-columns: repeat(3, 1fr)}
+   .rotate-340 { transform: rotate (330deg) !important; }
+   .no-effect:focus, .no-effect:active {
+      background-color: transparent !important;
+      border: none !important;
+   }
+   .no-effect:focus {outline: none !important}
+   .p-sidebar-mask { padding-top: 4.6rem !important ; z-index: 3 !important ;}
+   
+   @keyframes fade
 `
